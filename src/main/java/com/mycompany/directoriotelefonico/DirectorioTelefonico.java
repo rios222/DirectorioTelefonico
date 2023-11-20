7d891d2b38dc36d27afaacc56601aac9c6b1777a
rios222 <daniel.felipe.rios@correounivalle.edu.co>
2023-11-20 17:26:17 -0500

    se agrega el proyecto

---
+++ src/main/java/com/mycompany/directoriotelefonico/DirectorioTelefonico.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.directoriotelefonico;
+
+/**
+ *
+ * @author Dani
+ */
+public class DirectorioTelefonico {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
