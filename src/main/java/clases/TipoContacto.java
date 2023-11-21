c1d752b340f7c7417757f1543de520be5e520de6
rios222 <daniel.felipe.rios@correounivalle.edu.co>
2023-11-20 22:18:05 -0500

    comienza la clase del tipo de contacto

---
+++ src/main/java/clases/TipoContacto.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clases;
+
+/**
+ *
+ * @author Dani
+ */
+public class TipoContacto {
+    private int codigo;
+    private string nombre;
+    
+    public TipoContacto(){
+        
+    }
+    
+   public tipo (int codigo, string nombre){
+      this.codigo = codigo;
+      this.nombre = nombre;
+       
+   }
+    
+}
